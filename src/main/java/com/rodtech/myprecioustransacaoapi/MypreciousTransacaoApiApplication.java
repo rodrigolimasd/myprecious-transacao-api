c55de78773fc6b0623cd2aa300fc0d46829f88e8
Rodrigo Lima <rodrigolimasd@gmail.com>
2020-12-08 18:41:30 -0300

    primeiro commit

---
+++ src/main/java/com/rodtech/myprecioustransacaoapi/MypreciousTransacaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.rodtech.myprecioustransacaoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypreciousTransacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypreciousTransacaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodtech/myprecioustransacaoapi/MypreciousTransacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodtech.myprecioustransacaoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypreciousTransacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
